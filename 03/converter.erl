874710ea27fe6279c0ed6856de641bccd0c1de49
t1m0f3y <t.leonenko2001@gmail.com>
2023-03-10 20:43:16 +0700

    03 add to_rub2/1

--- 03/converter.erl
+++ 03/converter.erl
@@ -1,6 +1,6 @@
 -module(converter).
 
--export([to_rub/1]).
+-export([to_rub/1, to_rub2/1]).
 
 
 to_rub({usd, Amount}) when is_integer(Amount), Amount > 0 ->
@@ -44,3 +44,43 @@ to_rub({Wrong, Amount}) when is_integer(Amount), Amount > 0 ->
 to_rub(Error) ->
 	io:format("~p - bad args~n", [Error]),
 	{error, badarg}.
+
+
+to_rub2(Arg) ->
+	Result =
+		case Arg of
+			{usd, Amount} when is_integer(Amount), Amount > 0 ->
+				io:format("Convert ~p to rub, amount ~p~n", [usd, Amount]),
+				Converted = Amount * 75.5,
+				{ok, Converted};
+			{euro, Amount} when is_integer(Amount), Amount > 0 ->
+				io:format("Convert ~p to rub, amount ~p~n", [usd, Amount]),
+				Converted = Amount * 80,
+				{ok, Converted};
+			{lari, Amount} when is_integer(Amount), Amount > 0 ->
+				io:format("Convert ~p to rub, amount ~p~n", [usd, Amount]),
+				Converted = Amount * 29,
+				{ok, Converted};
+			{peso, Amount} when is_integer(Amount), Amount > 0 ->
+				io:format("Convert ~p to rub, amount ~p~n", [usd, Amount]),
+				Converted = Amount * 3,
+				{ok, Converted};
+			{krone, Amount} when is_integer(Amount), Amount > 0 ->	
+				io:format("Convert ~p to rub, amount ~p~n", [usd, Amount]),
+				Converted = Amount * 10,
+				{ok, Converted};
+			{_, Amount} when not(is_integer(Amount)) ->		
+				io:format("~p - number is not integer~n", [Amount]),
+				{error, badarg};
+			{_, Amount} when not(Amount > 0) ->
+				io:format("~p - number is less than 0~n", [Amount]),
+				{error, badarg};
+			{Wrong, Amount} when is_integer(Amount), Amount > 0 ->		
+				io:format("~p - convert is not supported~n", [Wrong]),
+				{error, badarg};
+			Error ->		
+				io:format("~p - bad args~n", [Error]),
+				{error, badarg}
+		end,
+	io:format("Result ~p~n", [Result]),
+	Result.
